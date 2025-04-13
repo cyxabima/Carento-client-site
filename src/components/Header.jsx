459dc86a5ed266340513e72893a8d884fa2a6d9d
cyxabima <ukashaanwerali@gmail.com>
2025-04-13 11:35:55 +0500

    Pages created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,16 @@ const Header = () => {
         <nav className='flex justify-between p-3 shadow-2xs h-18 items-center'>
             <div className='flex h-full items-center'>
                 <img src="/logo.svg" alt="" width={34} height={34} />
-                <h3>Rentora Go</h3>
+                <h3 className='ml-5'>Rentora Go</h3>
 
             </div>
             <ul className='hidden md:flex gap-16'>
                 <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
                 <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Contact</li>
                 <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>About</li>
-                {isLogged? 
-                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
-                :""}
+                {isLogged ?
+                    <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
+                    : ""}
             </ul>
 
             {
